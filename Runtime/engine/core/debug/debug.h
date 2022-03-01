8b5223d4a29caa59293bdbd1170183b549fcb361
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-03-01 17:13:25 +0300

    [Debug] Added a static assert for compile time assertion

--- Runtime/engine/core/debug/debug.h
+++ Runtime/engine/core/debug/debug.h
@@ -2,13 +2,16 @@
 #ifndef POLOS_CORE_DEBUG_H
 #define POLOS_CORE_DEBUG_H
 
+#include "utils/macro_util.h"
 
 #ifdef PL_DEBUG
 #	define ASSERTSTR(check, ...)		{ if (!(check)) { LOG_CORE_CRITICAL("Assertion failed at File: {0}, Line: {1}. \n Msg: {2}", __FILE__, __LINE__, __VA_ARGS__); __debugbreak();} }
 #	define ASSERT(check)				ASSERTSTR(check, "None")
+#	define STATIC_ASSERT(check)			static_assert(check, "Compile time assertion fail: " PL_STRINGIFY(check))
 #else
 #	define ASSERT(check, ...)
 #	define ASSERT(check)
+#	define STATIC_ASSERT(check)
 #endif
 
 #endif /* POLOS_CORE_DEBUG_H */
