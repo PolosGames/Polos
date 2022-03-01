3e48ff7e536889160f763bfd1da722d432e16101
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-03-01 18:09:24 +0300

    [Time] Clock's period changed to microseconds

--- Runtime/engine/core/time/clock.h
+++ Runtime/engine/core/time/clock.h
@@ -2,16 +2,17 @@
 #ifndef POLOS_CORE_TIME_CLOCK_H_
 #define POLOS_CORE_TIME_CLOCK_H_
 
-#include "utils/alias.h"
 #include <ratio>
+
+#include "utils/alias.h"
 
 namespace polos::time
 {
 	class Clock
 	{
-		using Period = std::nano;
+		using Period = std::micro;
 	public:
-		static void  Initialize();
+		static void  OnInitialize();
 		static int64 Now();
 	private:
 		static int64 m_Freq;
