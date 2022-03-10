8d5f41cb1604ab3c082797289f5226be55ab12de
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-03-10 14:24:31 +0300

    [Events] Added a engine_stop event for future use

---
+++ Runtime/engine/core/events/engine/engine_stop.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef POLOS_CORE_EVENTS_ENGINE_ENGINESTOP_H_
+#define POLOS_CORE_EVENTS_ENGINE_ENGINESTOP_H_
+
+#include "events/event.h"
+#include "utils/alias.h"
+
+namespace polos
+{
+	class engine_stop : public event<engine_stop>
+	{
+	public:
+		uint32 engine_event;
+
+		engine_stop() = default;
+		engine_stop(uint32 engine_event) 
+			: engine_event(engine_event)
+		{}
+	};
+}
+
+#endif /* POLOS_CORE_EVENTS_ENGINE_ENGINESTOP_H_ */
