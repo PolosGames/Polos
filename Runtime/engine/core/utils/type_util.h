4ee3cf1601372882179591e0a6fd956e6e76f392
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-03-10 20:19:43 +0300

    [Utils] Moved hash function definitions into the cpp file

--- Runtime/engine/core/utils/type_util.h
+++ Runtime/engine/core/utils/type_util.h
@@ -2,20 +2,15 @@
 #ifndef POLOS_CORE_UTILS_TYPEUTIL_H_
 #define POLOS_CORE_UTILS_TYPEUTIL_H_
 
-#include <string>
-
 #include "alias.h"
 
 namespace polos
 {
-	////////////////////		
-	/// DECLARATIONS ///
-	////////////////////
 
 	template<typename T>
-	std::string type_name()
+	char const* TypeName()
 	{
-		std::string name = typeid(T).name();
+		static std::string name = typeid(T).name();
 
 		// find the namespace colon
 		size_t name_pos = name.find_last_of(':');
@@ -35,43 +30,20 @@ namespace polos
 		return name.c_str();
 	}
 
-	// Polynomial rolling hash
-	size_t string_hash(std::string const& str);
+	int64 Hashit(char const* const str);
+	int64 Hashit(std::string const& str);
+	int64 Hashit(std::string&& str);
 
 	template<typename T>
-	size_t type_hash()
+	string_id TypeHash()
 	{
-		static std::string name = type_name<T>();
-		static size_t th = string_hash(name);
+		static char const* name = TypeName<T>();
+		static int64 th = Hashit(name);
 
 		return th;
 	}
 
-	///////////////////	
-	/// DEFINITIONS ///
-	///////////////////
-
-	inline size_t string_hash(std::string const& str)
-	{
-		uint32 p = 31;
-		uint32 m = 1'000'000'009;
-		size_t power_of_p = 1;
-		size_t hash_val = 0;
-
-		// Loop to calculate the hash value
-		// by iterating over the elements of string
-		for (char c : str)
-		{
-			// make the character lowercase
-			if (c >= 65 && c - 'a' < 0) c += 32;
-
-			hash_val = (hash_val + (c - 'a' + 1) * power_of_p) % m;
-			power_of_p = (power_of_p * p) % m;
-		}
-
-		//return positive remainder only
-		return (hash_val % m + m) % m;
-	}
+	inline string_id operator""_sid(char const* str, std::size_t _) { Hashit(str); }
 }
 
 
