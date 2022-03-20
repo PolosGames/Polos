53e48dfd7f4359c8f304fccd9b31ac284331b939
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-03-20 17:39:31 +0300

    [Build] Forgot to add PCH to the commit

---
+++ Runtime/engine/polos_pch.h
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cstring>
+#include <cstdint>
+#include <cstdlib>
+#include <memory>
+#include <mutex>
+#include <utility>
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+#include "core/debug/plassert.h"
+#include "core/debug/profiling.h"
+#include "core/utils/alias.h"
+#include "core/utils/feature.h"
+#include "core/log.h"
