2a774cd2831374f8a47ec5af0dc25e9eae07bff9
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-03-20 15:01:50 +0300

    [Build] CMakeLists.txt was missing

--- CMake/target_build.cmake
+++ CMake/target_build.cmake
@@ -11,23 +11,23 @@ function(build_options target)
         target_compile_options(${target} PRIVATE "-Wall")
     endif()
 
-    if(PL_DEBUG)
-        if (MSVC)
-            target_compile_options(${target} PRIVATE "/Od")
-        elseif (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR
-                CMAKE_C_COMPILER_ID STREQUAL "Clang" OR
-                CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
-
-            target_compile_options(${target} PRIVATE "-O0")
-        endif()
-    elseif(PL_RELEASE)
-        if (MSVC)
-            target_compile_options(${target} PRIVATE "/02")
-        elseif ($<C_COMPILER_ID:GNU> OR
-                $<C_COMPILER_ID:Clang> OR
-                $<C_COMPILER_ID:AppleClang>)
-            target_compile_options(${target} PRIVATE "-O3")
-        endif()
+    if (MSVC)
+        target_compile_options(${target}
+            PRIVATE $<$<CONFIG:Debug>: /Od /ZI>
+                    $<$<CONFIG:RelWithDebInfo>: /0t /ZI>
+                    $<$<CONFIG:Release>: /O2>
+                    $<$<CONFIG:MinSizeRel>: /O1>
+        )
+    elseif (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR
+            CMAKE_C_COMPILER_ID STREQUAL "Clang" OR
+            CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
+    
+        target_compile_options(${target}
+            PRIVATE $<$<CONFIG:Debug>: -O0 -g>
+                    $<$<CONFIG:RelWithDebInfo>: -O2 -g>
+                    $<$<CONFIG:Release>: -O2>
+                    $<$<CONFIG:MinSizeRel>: -O3>
+            )
     endif()
 
 endfunction()
