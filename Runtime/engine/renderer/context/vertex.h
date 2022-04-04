5970b40ccc4cac6a379adb4b2fd0b3205a57581e
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-04-04 19:40:59 +0300

    [Context] Vertex struct

---
+++ Runtime/engine/renderer/context/vertex.h
@@ -0,0 +1,15 @@
+#ifndef POLOS_VERTEX_H_
+#define POLOS_VERTEX_H_
+
+#include <glm/glm.hpp>
+
+namespace polos
+{
+    struct vertex
+    {
+        glm::vec3 position;
+        glm::vec4 color;
+    };
+}
+
+#endif //POLOS_VERTEX_H_
