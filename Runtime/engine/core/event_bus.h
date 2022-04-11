22b8fa23910b0ea1cbdf0323419de84660308202
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-04-11 17:39:15 +0300

    [Core] Removed the move member function of adding a Delegate
    Also moved the function definitions to another file

--- Runtime/engine/core/event_bus.h
+++ Runtime/engine/core/event_bus.h
@@ -3,9 +3,9 @@
 #ifndef POLOS_CORE_EVENTBUS_H_
 #define POLOS_CORE_EVENTBUS_H_
 
-#include "containers/delegate.h"
 #include "events/event.h"
 #include "containers/containers.h"
+#include "containers/delegate.h"
 
 namespace polos
 {
@@ -28,9 +28,6 @@ namespace polos
         static void SubscribeToEvent(const Delegate<void(event_type &)>& cback);
 
         template<class event_type>
-        static void SubscribeToEvent(Delegate<void(event_type &)> &&cback);
-
-        template<class event_type>
         static void UnsubscribeFromEvent(const Delegate<void(event_type &)>& cback);
 
     private:
@@ -38,56 +35,8 @@ namespace polos
 
         HashMap<event_id, DArray<EventSubscriber>> m_Callbacks;
     };
-
-    template<class event_type, typename ...Args>
-    inline void EventBus::RaiseEvent(Args&&... args)
-    {
-        auto& cbs = m_Instance->m_Callbacks;
-        if (cbs.count(event_type::id))
-        {
-            event_type e(args...);
-            event_id id = event_type::id;
-            for (auto& subscriber_function : cbs[id])
-            {
-                subscriber_function(e);
-            }
-        }
-    }
-
-    template<class event_type, class object_type, void(object_type::* method_ptr)(event_type &)>
-    inline void EventBus::SubscribeToEvent(object_type* const ptr)
-    {
-        auto& cbs = m_Instance->m_Callbacks;
-        auto del = Delegate<void(event_type&)>::template From<object_type, method_ptr>(
-                reinterpret_cast<object_type *>(ptr));
-        event_id id = event_type::id;
-        cbs.try_emplace(id).first->second.push_back(reinterpret_cast<const EventSubscriber&>(del));
-    }
-
-    template<class event_type>
-    inline void EventBus::SubscribeToEvent(const Delegate<void(event_type&)>& cback)
-    {
-        auto& cbs = m_Instance->m_Callbacks;
-        event_id id = event_type::id;
-        cbs.try_emplace(id).first->second.push_back(reinterpret_cast<EventSubscriber const&>(cback));
-    }
-
-    template<class event_type>
-    inline void EventBus::SubscribeToEvent(Delegate<void(event_type&)>&& cback)
-    {
-        auto &cbs = m_Instance->m_Callbacks;
-        event_id id = event_type::id;
-        cbs.try_emplace(id).first->second.push_back(reinterpret_cast<EventSubscriber&&>(cback));
-    }
-
-    template<class event_type>
-    inline void EventBus::UnsubscribeFromEvent(const Delegate<void(event_type&)>& cback)
-    {
-        auto &cbs = m_Instance->m_Callbacks;
-        event_id id = event_type::id;
-        cbs.at(id).erase(std::remove(cbs.at(id).begin(), cbs.at(id).end(), reinterpret_cast<const EventSubscriber&>(cback)), cbs.at(id).end());
-    }
-
 } // namespace polos
+
+#include "event_bus.inl"
 
 #endif /* POLOS_CORE_EVENTBUS_H_ */
---
+++ Runtime/engine/core/event_bus.inl
@@ -0,0 +1,43 @@
+namespace polos
+{
+    template<class event_type, typename ...Args>
+    inline void EventBus::RaiseEvent(Args&&... args)
+    {
+        auto& cbs = m_Instance->m_Callbacks;
+        if (cbs.count(event_type::id))
+        {
+            event_type e(args...);
+            event_id id = event_type::id;
+            for (auto& subscriber_function : cbs[id])
+            {
+                subscriber_function(e);
+            }
+        }
+    }
+    
+    template<class event_type, class object_type, void(object_type::* method_ptr)(event_type &)>
+    inline void EventBus::SubscribeToEvent(object_type* const ptr)
+    {
+        auto& cbs = m_Instance->m_Callbacks;
+        auto del = Delegate<void(event_type&)>::template From<object_type, method_ptr>(
+                reinterpret_cast<object_type *>(ptr));
+        event_id id = event_type::id;
+        cbs.try_emplace(id).first->second.push_back(reinterpret_cast<const EventSubscriber&>(del));
+    }
+    
+    template<class event_type>
+    inline void EventBus::SubscribeToEvent(const Delegate<void(event_type&)>& cback)
+    {
+        auto& cbs = m_Instance->m_Callbacks;
+        event_id id = event_type::id;
+        cbs.try_emplace(id).first->second.push_back(reinterpret_cast<EventSubscriber const&>(cback));
+    }
+    
+    template<class event_type>
+    inline void EventBus::UnsubscribeFromEvent(const Delegate<void(event_type&)>& cback)
+    {
+        auto &cbs = m_Instance->m_Callbacks;
+        event_id id = event_type::id;
+        cbs.at(id).erase(std::remove(cbs.at(id).begin(), cbs.at(id).end(), reinterpret_cast<const EventSubscriber&>(cback)), cbs.at(id).end());
+    }
+}
