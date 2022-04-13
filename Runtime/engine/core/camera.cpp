3372460c6036875f0a93c5c1f4b656d0f1d0c8ae
Kayra <44715451+kayraurfali@users.noreply.github.com>
2022-04-13 11:11:55 +0300

    [Core] Added a basic camera script

---
+++ Runtime/engine/core/camera.cpp
@@ -0,0 +1,88 @@
+#include "polos_pch.h"
+
+#include "camera.h"
+
+namespace polos
+{
+    constexpr float g_Yaw         = -90.0f;
+    constexpr float g_Pitch       =   0.0f;
+    constexpr float g_Speed       =   2.5f;
+    constexpr float g_Sensitivity =   0.1f;
+    constexpr float g_Zoom        =  45.0f;
+    
+    Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
+        : position{position},
+          front{glm::vec3{0.0f, 0.0f, -1.0f}},
+          up{{}},
+          right{{}},
+          worldUp(up),
+          yaw(yaw),
+          pitch(pitch),
+          movementSpeed(g_Speed),
+          mouseSensitivity(g_Sensitivity),
+          zoom(g_Zoom)
+    {
+        update_camera_vectors();
+    }
+    
+    void Camera::ProcessKeyboard(CameraMovement direction, float delta_time)
+    {
+        float velocity = movementSpeed * delta_time;
+        if (direction == kForward)
+            position += front * velocity;
+        if (direction == kBackward)
+            position -= front * velocity;
+        if (direction == kLeft)
+            position -= right * velocity;
+        if (direction == kRight)
+            position += right * velocity;
+    }
+    
+    void Camera::ProcessMouseMovement(float x_offset, float y_offset, GLboolean constrain_pitch)
+    {
+        x_offset *= mouseSensitivity;
+        y_offset *= mouseSensitivity;
+    
+        yaw   += x_offset;
+        pitch += y_offset;
+    
+        // make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (constrain_pitch)
+        {
+            if (pitch > 89.0f)
+                pitch = 89.0f;
+            if (pitch < -89.0f)
+                pitch = -89.0f;
+        }
+    
+        // update Front, Right and Up Vectors using the updated Euler angles
+        update_camera_vectors();
+    }
+    
+    void Camera::ProcessMouseScroll(float y_offset)
+    {
+        zoom -= (float)y_offset;
+        if (zoom < 1.0f)
+            zoom = 1.0f;
+        if (zoom > 45.0f)
+            zoom = 45.0f;
+    }
+    
+    glm::mat4 Camera::GetViewMatrix() const
+    {
+        return glm::lookAt(position, position + front, up);
+    }
+    
+    void Camera::update_camera_vectors()
+    {
+        // calculate the new Front vector
+        glm::vec3 p_front;
+        p_front.x = glm::cos(yaw) * glm::cos(pitch);
+        p_front.y = glm::sin(pitch);
+        p_front.z = glm::sin(yaw) * glm::cos(pitch);
+        p_front = glm::normalize(p_front);
+        // also re-calculate the Right and Up vector
+        right = glm::normalize(glm::cross(p_front, worldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        up    = glm::normalize(glm::cross(right, p_front));
+    }
+}
---
+++ Runtime/engine/core/camera.h
@@ -0,0 +1,66 @@
+#pragma once
+#ifndef POLOS_CAMERA_H_
+#define POLOS_CAMERA_H_
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace polos
+{
+    // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+    enum CameraMovement {
+        kForward,
+        kBackward,
+        kLeft,
+        kRight
+    };
+    
+    namespace globals
+    {
+        extern const float g_Yaw;
+        extern const float g_Pitch;
+        extern const float g_Speed;
+        extern const float g_Sensitivity;
+        extern const float g_Zoom;
+    }
+
+    // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+    class Camera
+    {
+    public:
+        // constructor with vectors
+        Camera(glm::vec3 position, glm::vec3 up, float yaw = globals::g_Yaw, float pitch = globals::g_Pitch);
+
+        // returns the view matrix calculated using Euler Angles and the LookAt Matrix
+        PL_NODISCARD glm::mat4 GetViewMatrix() const;
+        
+        // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+        void ProcessKeyboard(CameraMovement direction, float delta_time);
+        
+        // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+        void ProcessMouseMovement(float x_offset, float y_offset, GLboolean constrain_pitch = true);
+        
+        // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+        void ProcessMouseScroll(float y_offset);
+        
+        // camera Attributes
+        glm::vec3 position;
+        glm::vec3 front;
+        glm::vec3 up;
+        glm::vec3 right;
+        glm::vec3 worldUp;
+        // euler Angles
+        float yaw;
+        float pitch;
+        // camera options
+        float movementSpeed;
+        float mouseSensitivity;
+        float zoom;
+    private:
+        // calculates the front vector from the Camera's (updated) Euler Angles
+        void update_camera_vectors();
+    };
+}
+
+#endif //POLOS_CAMERA_H_
