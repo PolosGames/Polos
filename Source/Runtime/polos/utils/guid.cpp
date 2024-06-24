adef5845d968d0272040caf17d063e069c67df7d
sorbatdev <44715451+sorbatdev@users.noreply.github.com>
2024-06-24 19:37:46 +0300

    [upd] Remove stduuid dependency
    This thing would just create problem after problem, getting rid of it is a good idea.

--- Source/Runtime/polos/utils/guid.cpp
+++ Source/Runtime/polos/utils/guid.cpp
@@ -2,6 +2,10 @@
 
 namespace polos
 {
+    static std::random_device s_rd;
+    static std::mt19937_64 s_generator(s_rd());
+    static std::uniform_int_distribution<uint64_t> s_uniformDistrib;
+
     bool operator==(GUID const& p_Lhs, GUID const& p_Rhs)
     {
         return p_Lhs.guid == p_Rhs.guid;
@@ -9,6 +13,6 @@ namespace polos
 
     GUID GenerateGUID()
     {
-        return GUID{uuids::uuid_system_generator{}()};
+        return GUID{s_uniformDistrib(s_generator)};
     }
 }// namespace polos
