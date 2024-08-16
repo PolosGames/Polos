db28f7c19e6bb30a4a68aa8c9494b0fa7d432dbe
sorbatdev <44715451+sorbatdev@users.noreply.github.com>
2024-08-16 22:14:58 +0300

    [build] Handle third-party libraries with vcpkg

--- CMake/cache_entries.cmake
+++ CMake/cache_entries.cmake
@@ -2,3 +2,8 @@ if (WIN32)
     set(CMAKE_C_COMPILER cl.exe)
     set(CMAKE_CXX_COMPILER cl.exe)
 endif ()
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS True)
+set(BUILD_EDIR True)
+
+include("$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
